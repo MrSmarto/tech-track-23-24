0940022f8fabd1fa34c18bb85c1a2d6c782be127
MrSmarto <thijmensmarto@gmail.com>
2023-11-04 15:12:52 +0100

    Initial commit

---
+++ src/components/BarChart.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    onMount(() => {
+      console.log('BarChart component mounted');
+      // Hier zou je logica komen om de barchart te initialiseren
+    });
+  </script>
+  
+  <main>
+    <h1>Bar Chart Visualisatie</h1>
+    <!-- De barchart zou hier gerenderd worden -->
+  </main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import BarChart from '../components/BarChart.svelte';
+  </script>
+  
+  <main>
+    <h1>Welkom bij de Tech Track Visualisatie</h1>
+    <BarChart />
+  </main>
