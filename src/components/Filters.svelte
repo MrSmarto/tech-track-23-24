8986cd40a33a11d95696af850bb9284e5eb32b48
MrSmarto <thijmensmarto@gmail.com>
2023-11-11 00:19:30 +0100

    test

--- src/components/Filters.svelte
+++ src/components/Filters.svelte
@@ -1,4 +1,4 @@
-<script>
+s<script>
     export let onFilterChange;
   
     function handleRegionChange(event) {
@@ -8,15 +8,15 @@
     function handleTimeChange(event) {
       onFilterChange({ type: 'time', value: event.target.value });
     }
-</script>
+  </script>
   
   <select on:change={handleRegionChange}>
     <option value="">Kies een regio</option>
-    {/* Voeg hier opties toe voor elke regio */}
+    <!-- Voeg hier opties toe voor elke regio -->
   </select>
   
   <select on:change={handleTimeChange}>
     <option value="">Kies een periode</option>
-    {/* Voeg hier opties toe voor tijdperiodes */}
-</select>
+    <!-- Voeg hier opties toe voor tijdperiodes -->
+  </select>
   
